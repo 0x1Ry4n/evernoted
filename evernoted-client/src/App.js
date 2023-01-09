87d66b824249801eb62fb46af53defab3e54137a
Ryan <ryanlucas2018gz@gmail.com>
2023-01-09 01:24:24 -0300

    App entry point

---
+++ evernoted-client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./App.scss";
+import RouteMiddleware from './routes';
+
+const App = () => (
+  <div>
+    <RouteMiddleware/>
+  </div>
+)
+
+export default App;
